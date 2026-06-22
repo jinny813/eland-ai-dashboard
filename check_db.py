314ad208202a80f79690e32cd1787e8b89302441
jinny813 <LEE_HWAJIN03@eland.co.kr>
2026-06-22 14:00:23 +0900

    chore: remove test files

--- check_db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('database/product_master.db')
-c = conn.cursor()
-c.execute("SELECT * FROM products WHERE style_code='P4H21WBL090'")
-print('P4H21WBL090:', c.fetchall())
-c.execute("SELECT * FROM products WHERE style_code='P1G21WBL020'")
-print('P1G21WBL020:', c.fetchall())
